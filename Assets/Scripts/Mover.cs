3d6ded18e445f63b5be4f65e9975c803b2f1874d
Marc Sevilleja <marc@sevilayha.com>
2020-04-25 18:42:17 -0700

    Created simple script to move player to target position utilizing nav mesh agent

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.position;
+    }
+}
