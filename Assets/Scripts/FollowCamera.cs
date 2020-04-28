8fc3aa0ef83f763f87ba388b698fa7f436a3c6a2
Marc Sevilleja <marc@sevilayha.com>
2020-04-27 17:50:56 -0700

    Added camera rotation and movement
    Camera will follow player and will rotate around player when pressing Q/E but rotating the empty gameObject that follows player around

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float rotateSpeed = 5f;
+
+    private float _rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        transform.position = target.position;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            _rotation += rotateSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, _rotation, 0);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            _rotation -= rotateSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, _rotation, 0);
+        }
+    }
+
+}
