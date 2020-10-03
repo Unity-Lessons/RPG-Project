c9b98b9b388036523d1a734cc28b3dc891eef9e7
Marc Sevilleja <marc@sevilayha.com>
2020-10-02 18:01:20 -0700

    Made mover more flexibile

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -25,21 +25,25 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
-            MoveToCursor();
-        
+        //if (Input.GetMouseButtonDown(0))
+        //    MoveToCursor();
 
         UpdateAnimator();
     }
 
-    void MoveToCursor()
+    private void MoveToCursor()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         bool hasHit = Physics.Raycast(ray, out hit);
 
         if (hasHit)
-            agent.destination = hit.point;
+            GetPoint(hit.point);
+    }
+
+    public void GetPoint(Vector3 destination)
+    {
+        agent.destination = destination;
     }
 
     private void UpdateAnimator()
