6a2aac2c04e7866c99e217334caf207e0b7f0822
Marc Sevilleja <marc@sevilayha.com>
2020-10-02 19:09:37 -0700

    Implemented basic combat target on target click

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,21 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
+using System;
 
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
         Mover mover;
+        Fighter fighter;
 
         // Start is called before the first frame update
         void Start()
         {
             mover = GetComponent<Mover>();
+            fighter = GetComponent<Fighter>();
         }
 
         // Update is called once per frame
         void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+
+                if (target == null) continue;
+                    
+                if (Input.GetMouseButtonDown(0))
+                    fighter.Attack(target);
+            }
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButtonDown(0))
                 MoveToCursor();
@@ -24,12 +48,16 @@ namespace RPG.Control
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
             if (hasHit)
                 mover.GetPoint(hit.point);
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
